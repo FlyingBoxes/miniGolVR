e026fa089122bf9af8cd9edd6def993b44824256
unknown <john.briggs@mavs.uta.edu>
2021-10-20 15:51:05 -0500

    test first commit

---
+++ Assets/TestActionController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TestActionController : MonoBehaviour
+{
+    private ActionBasedController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<ActionBasedController>();
+
+        // basic way of storing input. Stores it within a variable.
+        bool isPressed = controller.selectAction.action.ReadValue<bool>();
+
+        // call function (Action_perfomed) once input is detected.
+        controller.selectAction.action.performed += Action_perfomed;
+    }
+
+    private void Action_perfomed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        Debug.Log("Select Button Pressed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# MiniGolVR
+Unity Project for MiniGolVR game.  
+Unity Version: 2020.3.18.f1
