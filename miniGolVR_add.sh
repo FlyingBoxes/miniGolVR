e6d501daf2d78aa5cdc195b74dc75504412dc8d5
FlyingBoxes <58245202+FlyingBoxes@users.noreply.github.com>
2021-10-25 20:32:47 -0500

    Update miniGolVR_add.sh

--- miniGolVR_add.sh
+++ miniGolVR_add.sh
@@ -1,7 +1,11 @@
 git add ./Assets/Scripts/
+git add ./Assets/Scripts.meta
 git add ./Assets/Scenes/
+git add ./Assets/Scenes.meta
 git add ./Assets/Settings/
+git add ./Assets/Settings.meta
 git add ./Assets/Materials/
+git add ./Assets/Materials.meta
 git add ./Packages/
 git add ./ProjectSettings/
 git add ./.gitignore
