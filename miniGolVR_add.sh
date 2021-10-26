e9676398dea2d5a7224f55f220691906a55a4df7
FlyingBoxes <58245202+FlyingBoxes@users.noreply.github.com>
2021-10-25 20:21:37 -0500

    Update miniGolVR_add.sh

--- miniGolVR_add.sh
+++ miniGolVR_add.sh
@@ -1,26 +1,3 @@
-git add ./Assets/Materials/
-git add ./Assets/Scenes/
-git add ./Assets/Scripts/
-git add ./Assets/Settings/
-git add ./Assets/Materials.meta
-git add ./Assets/'New Terrain 1.asset'
-git add ./Assets/'New Terrain 1.asset.meta'
-git add ./Assets/'New Terrain.asset'
-git add ./Assets/'New Terrain.asset.meta'
-git add ./Assets/Readme.asset
-git add ./Assets/Scenes.meta
-git add ./Assets/Scripts.meta
-git add ./Assets/Settings.meta
-git add ./Assets/TestActionController.cs
-git add ./Assets/TestActionController.cs.meta
-git add ./Assets/mygrass.terrainlayer
-git add ./Assets/mygrass.terrainlayer.meta
-git add ./Assets/myground.terrainlayer
-git add ./Assets/myground.terrainlayer.meta
-git add ./Assets/'myscree 1.terrainlayer'
-git add ./Assets/'myscree 1.terrainlayer.meta'
-git add ./Assets/myscree.terrainlayer
-git add ./Assets/myscree.terrainlayer.meta
 git add ./Packages/
 git add ./ProjectSettings/
 git add ./.gitignore
