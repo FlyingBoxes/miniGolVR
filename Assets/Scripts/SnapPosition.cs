62e0b650e7699683382ac9a608b0abb80aa0f1c1
jeremiagusti <jeremia.gusti@gmail.com>
2021-11-03 19:17:48 -0500

    add minigolf gimmicks

--- Assets/Scripts/SnapPosition.cs
+++ Assets/Scripts/SnapPosition.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class SnapPosition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter(Collider other)
     {
+        
+        other.transform.parent.position = transform.position;
         Debug.Log("Hitting trigger");
     }
 }
